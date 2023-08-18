5821e426b9cfe958b04079e7dc54b3740509cfcd
yilinya0809 <leejwblack@snu.ac.kr>
2023-08-18 17:31:53 +0900

    wip

--- main.py
+++ main.py
@@ -1,2 +1,102 @@
-"""
-"""
+import fym
+import matplotlib.pyplot as plt
+import numpy as np
+from fym.utils.rot import angle2quat
+from scipy.integrate import quad
+from scipy.optimize import minimize
+
+from simpledyn import simpleLC62
+
+class TrajOpt(fym.BaseEnv):
+    ENV_CONFIG = {
+        "fkw": {
+            "dt": 0.01,
+            "max_t": 10,
+        },
+        "plant": {
+            "init": {
+                "pos": np.vstack((0, 0, 0)),
+                "vel": np.vstack((0, 0, 0)),
+            },
+        },
+    }
+
+    
+
+    def __init__(self, env_config={}):
+        env_config = safeupdate(self.ENV_CONFIG, env_config)
+        super().__init__(**env_config["fkw"])
+        self.plant = simpleLC62(env_config["plant"])
+        self.tf = env_config["fkw"]["max_t"]
+        self.cruise_height = 10
+        self.cruise_speed = 45
+
+
+    def step(self):
+        env_info, done = self.update()
+        return done, env_info
+
+    def observation(self):
+        return self.observe_flat()
+
+    
+    def set_dot(self, t):
+
+        # TODO
+        # U = 
+        self.plant.set_dot(t, U)
+
+
+        env_info = {
+            "t": t,
+            **self.observe_dict(),
+            "U": U,
+        }
+
+        return env_info
+
+    def minimize_cost(
+        self,
+        tf,
+        U0={"F_r": 0, "F_p": 0, "theta": 0},
+        method="SLSQP",
+        options={"disp": False, "ftol": 1e-10},
+    ):
+
+        U0 = list(U0.values())
+        result = minimize(
+            self.performance_index,
+            U0,
+            args=(tf,),
+            method=method,
+            options=options,
+        )
+        
+        U = np.vstack((result.x))
+        return U
+
+    def performance_index(self, U, tf):
+        energy = self.energy(U)
+        J = quad(energy, 0, tf)
+        return J
+
+    def energy(self, U):
+
+       pos, vel = self.observe_list()
+       Vx, Vz = np.ravel(vel)
+
+       F_r, F_p, theta = np.ravel(U)
+
+       P_r = F_r * Vz
+       P_p = F_p * Vx
+       P = P_r + P_p
+       return P
+    
+   def Boundary_cond(
+       self,
+       fixed={"h", "V"},
+   ):
+
+       # TODO
+       # 
+       return X_trim, U_trim
--- simpledyn.py
+++ simpledyn.py
@@ -1,3 +1,172 @@
 """
-Simple Dynamic Model
+Simple Longitudinal Dynamic Model of LC62-50B
 """
+
+import fym
+import numpy as np
+from numpy import cos, sin
+
+
+class simpleLC62(fym.BaseEnv):
+
+    g = 9.81
+    m = 41.97
+
+    S1 = 0.2624  # [m^2]
+    S2 = 0.5898  # [m^2]
+    S = S1 + S2
+    St = 0.06894022  # [m^2]
+    c = 0.551  # Main wing chord length [m]
+    b = 1.1  # Main wing half span [m]
+    d = 0.849  # Moment arm length [m]
+
+    tables = {
+        "alp": np.deg2rad(np.array([0, 2, 4, 6, 8, 10, 12, 14, 16])),  # [rad]
+        "CL": np.array(
+            [0.1931, 0.4075, 0.6112, 0.7939, 0.9270, 1.0775, 0.9577, 1.0497, 1.0635]
+        ),
+        "CD": np.array(
+            [0.0617, 0.0668, 0.0788, 0.0948, 0.1199, 0.1504, 0.2105, 0.2594, 0.3128]
+        ),
+        "Cm": np.array(
+            [
+                0.0406,
+                0.0141,
+                -0.0208,
+                -0.0480,
+                -0.2717,
+                -0.4096,
+                -0.1448,
+                -0.2067,
+                -0.2548
+            ]
+        ),
+        "cmd": np.array(
+            [
+                0,
+                0.2,
+                0.255,
+                0.310,
+                0.365,
+                0.420,
+                0.475,
+                0.530,
+                0.585,
+                0.640,
+                0.695,
+                0.750,
+            ]
+        ),
+        "th_p": np.array(
+            [
+                0,
+                1.39,
+                4.22,
+                7.89,
+                12.36,
+                17.60,
+                23.19,
+                29.99,
+                39.09,
+                46.14,
+                52.67,
+                59.69,
+            ]
+        ),
+        "tq_p": np.array(
+            [
+                0, 
+                0.12, 
+                0.35, 
+                0.66, 
+                1.04, 
+                1.47, 
+                1.93, 
+                2.50, 
+                3.25, 
+                3.83, 
+                4.35, 
+                4.95,
+            ]
+        ),
+        "th_r": [-19281, 36503, -992.75, 0],
+        "tq_r": [-6.3961, 12.092, -0.3156, 0],
+    }
+
+    control_limits = {
+        "cmd": (0, 1),
+        "dela": np.deg2rad((-10, 10)),
+        "dele": np.deg2rad((-10, 10)),
+        "delr": np.deg2rad((-10, 10)),
+    }
+
+    ENV_CONFIG = {
+        "init": {
+            "pos": np.zeros((2, 1)),
+            "vel": np.zeros((2, 1)),
+        },
+    }
+
+    def __init__(self, env_cinfig={}):
+        env_config = safeupdate(self.ENV_CONFIG, env_config)
+        super().__init__()
+        self.pos = fym.BaseSystem(env_config["init"]["pos"])
+        self.vel = fym.BaseSystem(env_config["init"]["vel"])
+
+
+    def set_dot(self, t, U):
+        pos, vel = self.observe_list()
+        F_w = self.get_Fw(pos, vel)
+        Fx_w, Fz_w = np.ravel(F_w)
+        F_r, F_p, theta = np.ravel(U)
+
+        R = np.array(
+            [
+                [cos(theta), -sin(theta)], 
+                [sin(theta),  cos(theta)]
+            ]
+        )
+
+        Vx_dot = (F_p + Fx_w)/self.m - self.g * sin(theta)
+        Vz_dot = (-F_r + Fz_w)/self.m + self.g * cos(theta)
+      
+        self.pos.dot = R @ vel
+        self.vel.dot = np.vstack((Vx_dot, Vz_dot))
+
+
+    def get_Fw(self, pos, vel):
+        x, z = np.ravel(pos)
+        Vx, Vz = np.ravel(vel)
+        V = np.linalg.norm(vel)
+
+        rho = self.get_rho(-z) 
+        qbar = 0.5 * rho * V**2
+        alp = np.arctan2(Vz, Vx)
+        CL, CD, _ = self.aero_coeff(alp)
+        S = self.S
+
+        Fx_w = -qbar * S * CD
+        Fz_w = -qbar * S * CL
+
+        F_w = np.vstack((Fx_w, Fz_w))
+        return F_w
+
+
+    def get_rho(self, altitude):
+        pressure = 101325 * (1 - 2.25569e-5 * altitude) ** 5.25616
+        temperature = 288.14 - 0.00649 * altitude
+        return pressure / (287 * temperature)
+
+
+    def aero_coeff(self, alp):
+        CL = np.interp(alp, self.tables["alp"], self.tables["CL"])
+        CD = np.interp(alp, self.tables["alp"], self.tables["CD"])
+        Cm = np.interp(alp, self.tables["alp"], self.tables["Cm"])
+        return np.vstack((CL, CD, Cm))
+
+
+
+
+
+        
+
