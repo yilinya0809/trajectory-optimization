5805c16e7949c139d11b2bb176722e626e0188c2
yilinya0809 <leejwblack@snu.ac.kr>
2023-09-06 17:43:03 +0900

    wip

--- aero_coeff.m
+++ aero_coeff.m
@@ -5,12 +5,8 @@ function [CL, CD, Cm] = aero_coeff(alpha)
     CL_interp = casadi.interpolant('CL_interp','bspline',{alp},C_L);
     CD_interp = casadi.interpolant('CD_interp','bspline',{alp},C_D);
     Cm_interp = casadi.interpolant('Cm_interp','bspline',{alp},C_m);
-%     CL = full(CL_interp(alpha));
-%     CD = full(CD_interp(alpha));
-%     Cm = full(Cm_interp(alpha));
-
-    CL = 0; 
-    CD = 0; 
-    Cm = 0;
+    CL = full(CL_interp(alpha));
+    CD = full(CD_interp(alpha));
+    Cm = full(Cm_interp(alpha));
 
 end
